7b1990d0bf11b7c1f29a73668bdd4dde9c5d2eee
Mustafa Yasin Çelik <celikmustafayasin@gmail.com>
2022-02-26 02:57:28 +0300

    Update

--- DownNotifier/MvcWebUI/Startup.cs
+++ DownNotifier/MvcWebUI/Startup.cs
@@ -57,7 +57,7 @@ namespace MvcWebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=App}/{action=Index}/{id?}");
             });
         }
     }
